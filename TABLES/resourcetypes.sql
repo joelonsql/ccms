1d59954618259d094c90913b7783660e9f2dfc64
Joel Jakobsson <joel@compiler.org>
2020-04-13 17:55:16 +0200

    Delete ResourceTypes replaced by Beds/Docs

--- TABLES/resourcetypes.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE ResourceTypes (
-ResourceTypeID integer NOT NULL GENERATED ALWAYS AS IDENTITY,
-ResourceType text NOT NULL,
-ResourceName text NOT NULL,
-PRIMARY KEY (ResourceTypeID),
-UNIQUE (ResourceType),
-UNIQUE (ResourceName)
-);
